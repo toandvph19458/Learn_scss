55bc35f2576c9bd277ea29adbf768a2331cabb18
toandvph19458 <105204332+toandvph19458@users.noreply.github.com>
2023-08-29 11:05:30 +0700

    scss

--- layout/header.php
+++ layout/header.php
@@ -14,7 +14,7 @@
 
     <!--CSS-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/css/uikit.min.css" />
-    <link rel="stylesheet" href="style.css?>">
+    <link rel="stylesheet" href="style.scss">
     <!--JS-->
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.22/dist/js/uikit-icons.min.js"></script>
