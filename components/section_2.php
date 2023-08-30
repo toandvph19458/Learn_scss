3dea500c2f708e1c4263f3373e18d45345466945
nguyen <125894255+glexiv808@users.noreply.github.com>
2023-08-30 10:14:17 +0700

    section_2

---
+++ components/section_2.php
@@ -0,0 +1,21 @@
+<section class="section_2">
+        <div class="banner">
+            <img src="./images/letter-1.png" alt="">
+        </div>
+        <div class="content">
+            <div class="title">
+                <h6>THƯ NGỎ</h6>
+                <h3>Chúng tôi yêu những gì chúng tôi làm và mong được làm việc với bạn!</h3>
+            </div>
+            <p>GMI là một đơn vị chuyên nghiệp trong lĩnh vực sản xuất và phân phối các sản phẩm tiêu âm và các sản phẩm
+                trang trí nhập khẩu với nhiều loại chất liệu và mẫu mã đa dạng. Nhiệm vụ hàng ngày của chúng tôi là thúc
+                đẩy sự đổi mới và nghiên cứu các giải pháp tiêu âm, kết hợp những điều này để tạo ra khả năng thiết kế
+                không giới hạn. Sử dụng công nghệ là cốt lõi của một mạng lưới đối tác vững chắc, chúng tôi pha trộn các
+                khía cạnh tốt nhất của thiết kế, kỹ thuật và dịch vụ khách hàng, cung cấp một lựa chọn các sản phẩm kết
+                hợp hiệu suất tiêu âm cao và hiệu ứng hình ảnh độc đáo. Khách hàng là người truyền cảm hứng cho chúng
+                tôi và chúng tôi yêu cảm hứng. Để đáp ứng tốt hơn nhu cầu của khách hàng, chúng tôi đã nghiên cứu và cập
+                nhật nhiều sản phẩm mới nhằm tối ưu hóa các giải pháp thiết kế và sản xuất để chúng tối có thể cung cấp
+                cho khách hàng những dịch vụ tốt nhất. Chúng tôi mong muốn góp phần xây dựng cộng đồng kiến trúc và tạo
+                nên những công trình kiến trúc mang tính đột phá và toàn diện nhất.</p>
+        </div>
+</section>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php require "layout/header.php"; ?>
+<?php require "components/section_2.php"; ?>
 <?php require "components/section_3.php"; ?>
 <?php require "layout/footer.php"; ?>
